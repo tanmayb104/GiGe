8a283af836b69a916803be29292bd3a50a02c4b8
Tanmay Bansal <59735148+tanmayb104@users.noreply.github.com>
2021-03-01 15:15:54 +0530

    Setting file changed

--- Backend/gige/gige/settings.py
+++ Backend/gige/gige/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'zs*vo8@=3=nt4#s_=fj$x&6(6na*tf@q3+-hst73%_67&esb_%'
+SECRET_KEY = '1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
