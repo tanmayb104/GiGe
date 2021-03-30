26f05586ed4a8f191dff75c298cd3d11f355b459
atulragarwal <atul.agarwal2019@vitstudent.ac.in>
2021-03-30 17:47:23 +0530

    Review

--- Backend/gige/gige/settings.py
+++ Backend/gige/gige/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'gige.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'gige',
+        'NAME': 'GiGe',
         'USER': 'postgres',
         'PASSWORD': '1234',
         'HOST':'localhost',
