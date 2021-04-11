b73d49226312bb433dbc4045f04f8e8dabce640b
Tanmay <tanmaybansal104@gmail.com>
2021-04-11 21:19:45 +0530

    Profile linked

--- Backend/gige/store/models.py
+++ Backend/gige/store/models.py
@@ -10,23 +10,23 @@ class Item(models.Model):
     
     name = models.CharField(max_length=200)
     description = models.CharField(max_length=500)
-    item_pic = models.ImageField(upload_to ='item_pic')
-    owner = models.ForeignKey(Profile, on_delete=models.SET_NULL, blank=True, null=True)
-    price = models.IntegerField()
-    digital = models.BooleanField()
-    sold = models.BooleanField()
-    #category
+    # item_pic = models.ImageField(upload_to ='item_pic')
+    # owner = models.ForeignKey(Profile, on_delete=models.SET_NULL, blank=True, null=True)
+    # price = models.IntegerField()
+    # digital = models.BooleanField()
+    # sold = models.BooleanField()
+    # #category
 
-    def __str__(self):
-        return self.name
+    # def __str__(self):
+    #     return self.name
 
 
 class Transaction(models.Model):
 
     item = models.OneToOneField(Item, on_delete=models.SET_NULL, blank=True, null=True)
-    customer = models.ForeignKey(Profile, on_delete=models.SET_NULL, blank=True, null=True)
-    date_ordered = models.DateTimeField(auto_add_now=True)
-    transaction_id = models.CharField(max_length=200)
+    # customer = models.ForeignKey(Profile, on_delete=models.SET_NULL, blank=True, null=True)
+    # date_ordered = models.DateTimeField(auto_add_now=True)
+    # transaction_id = models.CharField(max_length=200)
 
-    def __str__(self):
-        return self.transaction_id
+    # def __str__(self):
+    #     return self.transaction_id
